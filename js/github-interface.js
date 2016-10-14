b21840e86a110200613cd950bc81cacdd26547af
Molly LeCompte <mollyklecompte@gmail.com>
2016-10-14 16:20:35 -0700

    add to github-interface and index files so that submitted user's name appears at top of repository list.

--- js/github-interface.js
+++ js/github-interface.js
@@ -4,9 +4,9 @@ $(document).ready(function() {
   var currentUserObject = new User();
   $('#user-form').submit(function(event) {
     event.preventDefault();
-    $('.showRepos').show();
-
     var username = $('#user-name').val();
+    $('#user').text(username);
+    $('.showRepos').show();
     currentUserObject.getRepos(username);
 
   });
